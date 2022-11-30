093ca9c561aad1eee81ca06a3f4608c9aab22369
Arseny Zhalnin <mrzhalnin@gmail.com>
2022-12-01 01:07:17 +0300

    Add Additionally Task

---
+++ Additionally/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+int N = new Random().Next(3, 1001);
+int[] array = new int[N];
+for (int i = 0; i < array.Length; i++)
+array[i] = new Random().Next(1, 1001); 
+int s = array[0] + array[1] + array[2];
+for (int a = 1; a < N - 1; a++)
+if (array[a-1]+array[a]+array[a+1]>s) s = array[a-1]+array[a]+array[a+1];
+
+for (int j = 0; j < array.Length; j++)
+Console.Write($"{array[j]}, "); //выводил массив для проверки
+Console.WriteLine();
+Console.WriteLine($"Maximum number of berries: {s}");
